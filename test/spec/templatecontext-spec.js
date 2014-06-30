86d55bd2e0b83cc20f1fc24108b3b8ef76e07b7d
goliatone <burgosemi@gmail.com>
2014-06-30 11:00:02 -0400

    TEST: Remove jquery dep.

--- test/spec/templatecontext-spec.js
+++ test/spec/templatecontext-spec.js
@@ -1,23 +1,12 @@
-/*global define:true, describe:true , it:true , expect:true, 
+/*global define:true, describe:true , it:true , expect:true,
 beforeEach:true, sinon:true, spyOn:true , expect:true */
 /* jshint strict: false */
-define(['templatecontext', 'jquery'], function(templatecontext, $) {
+define(['templatecontext'], function(templatecontext) {
 
     describe('just checking', function() {
 
         it('templatecontext should be loaded', function() {
             expect(templatecontext).toBeTruthy();
-            var templatecontext = new templatecontext();
-            expect(templatecontext).toBeTruthy();
         });
-
-        it('templatecontext should initialize', function() {
-            var templatecontext = new templatecontext();
-            var output   = templatecontext.init();
-            var expected = 'This is just a stub!';
-            expect(output).toEqual(expected);
-        });
-        
     });
-
 });
